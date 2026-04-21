858284f3ae150d498c44fdfccb234368e8438b28
Jannie Ngoy Ituka <janniengoyituka@icloud.com>
2026-04-21 21:44:26 +0200

    slideshow fonctionnel, captions à changer seulement

---
+++ script/slideshow.js
@@ -0,0 +1,27 @@
+let slideIndex = 0;
+
+function showSlides() {
+    let slides = document.querySelectorAll(".slide");
+
+    // 1. On cherche l'image qui a actuellement la classe "active"
+    let currentSlide = document.querySelector(".slide.active");
+
+    // 2. Si elle existe, on lui retire la classe (elle va fade out)
+    if (currentSlide) {
+        currentSlide.classList.remove("active");
+    }
+
+    // 3. On calcule l'index de la suivante
+    slideIndex++;
+    if (slideIndex > slides.length) { 
+        slideIndex = 1; 
+    }
+
+    // 4. On ajoute "active" à la nouvelle (elle va fade in)
+    slides[slideIndex - 1].classList.add("active");
+
+    // On relance
+    setTimeout(showSlides, 4000); 
+}
+
+showSlides();
